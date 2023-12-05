22e0deb74360febbd314f1f5547563ab038337da
Nilam Tambe <111958530+nilamtambe@users.noreply.github.com>
2023-12-05 11:02:29 +0530

    Add files via upload

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard';
+import {Routes,Route} from "react-router-dom";
+import CreateTextAd from '../components/CreateTextAd';
+import CreateMediaAd from '../components/CreateMediaAd';
+import Ads from '../components/Ads';
 
+function index() {
+  return (
+    <div>
+     <Routes>
+        <Route path='/' element={<Dashboard/>} />
+        <Route path='/ads' element={<Ads/>} />
+        <Route path='/createmediaad' element={<CreateMediaAd/>} />
+        <Route path='/createtextad' element={<CreateTextAd/>} />
+       
+     </Routes>
+    </div>
+  )
+}
+
+export default index
